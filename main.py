41548f390a9e5e74ee1406767d6bb47a2537f1a7
E124BG <graak@live.fr>
2023-02-22 17:54:30 +0100

    plan + generate_password function created

---
+++ main.py
@@ -0,0 +1,8 @@
+import random
+import string
+
+#ask user for min length of password
+#ask if user wants special characters (#$%...)
+
+def generate_password(min_length, numbers=True, special_characters=True):
+    pass
