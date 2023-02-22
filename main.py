0d0282040c8321521403fc8ca01786b6c9840eff
E124BG <graak@live.fr>
2023-02-22 18:15:52 +0100

    added special chars and numbers checks

--- main.py
+++ main.py
@@ -5,4 +5,43 @@ import string
 #ask if user wants special characters (#$%...)
 
 def generate_password(min_length, numbers=True, special_characters=True):
-    pass
+    
+    
+    letters = string.ascii_letters      #abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+    digits = string.digits              #0123456789
+    special_chars = string.punctuation  #!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+    
+    password_chars = letters #always include letters
+    if numbers :
+        password_chars += digits
+    if special_characters :
+        password_chars += special_chars
+        
+    
+    print(password_chars)
+    
+    password = ""
+    meets_criteria = False
+    has_number = False
+    has_special = False
+    
+    while not meets_criteria or len(password) < min_length:
+        #add one char to the password string
+        new_char = random.choice(password_chars)
+        pwd += new_char
+        
+        if new_char in digits:
+            has_number = True
+        elif new_char in special_chars:
+            has_special = True
+            
+        meets_criteria = True               #make it true and then try to prove it false
+        if numbers:                         #false if supposed to have numbers but doesn't have any
+            meets_criteria = has_number
+        if special_characters:              #false if supposed to have special chars but either failed before (meets_criteria is already False) or doesn't have special chars
+            meets_criteria = meets_criteria and has_special
+        
+            
+
+generate_password(10) #will create a password of 10 chars with numbers and special chars
+generate_password(10, True, False) #will create a password with numbers and no special chars
